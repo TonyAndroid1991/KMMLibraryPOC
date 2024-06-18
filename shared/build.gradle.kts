be0b7283a5d820b1fb506c42377935eff3827cbb
TonyAndroid1991 <jantoniosanchez1991@gmail.com>
2024-06-18 12:02:17 +0200

    first commit

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,34 +1,61 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinCocoapods)
+    id("maven-publish")
 }
 
+val libVersion = "0.0.1"
+
+group = "com.example.kmmlibrarypoc"
+version = libVersion
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
+    targetHierarchy.default()
+
     androidTarget {
+        publishLibraryVariants("release")
         compilations.all {
             kotlinOptions {
                 jvmTarget = "1.8"
             }
         }
     }
-    
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
+
+    val iosFrameworkName = "shared"
+    val xcf = XCFramework()
+    val iosTargets = listOf(iosX64(), iosArm64(), iosSimulatorArm64())
+
+    iosTargets.forEach {
         it.binaries.framework {
-            baseName = "shared"
-            isStatic = true
+            baseName = iosFrameworkName
+            xcf.add(this)
+        }
+    }
+
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = libVersion
+        ios.deploymentTarget = "14.1"
+        framework {
+            baseName = iosFrameworkName
         }
     }
 
     sourceSets {
-        commonMain.dependencies {
-            //put your multiplatform dependencies here
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
         }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
         }
     }
 }
@@ -37,13 +64,52 @@ android {
     namespace = "com.example.kmmlibrarypoc"
     compileSdk = 34
     defaultConfig {
-        minSdk = 24
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        minSdk = 23
     }
 }
+
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            groupId = "com.kingmakers.analytics"
+        }
+
+        create<MavenPublication>("iOSFramework") {
+            artifactId = "shared-ios"
+            artifact("$buildDir/cocoapods/publish/shared.zip")
+        }
+
+        repositories {
+            maven {
+                name="com.kingmakers.analytics"
+                url = uri("https://pkgs.dev.azure.com/BetagyDevOps/Kingmakers-Android/_packaging/com.kingmakers.analytics/maven/v1")
+                credentials {
+                    username = "${project.properties["kingmakersUser"]}"
+                    password = "${project.properties["kingmakersToken"]}"
+                }
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+    }
+}
+
+tasks.register<Zip>("buildIosFramework") {
+    dependsOn("podPublishReleaseXCFramework")
+    from(layout.buildDirectory.dir("cocoapods/publish/release/"))
+    destinationDirectory.set(layout.buildDirectory.dir("cocoapods/publish/"))
+    archiveFileName.set("shared.zip")
+}
+
+tasks.register("publishIosFrameworkToMaven") {
+    dependsOn("buildIosFramework")
+    dependsOn("publishIOSFrameworkPublicationToCom.kingmakers.analyticsRepository")
+    tasks.findByName("publishIOSFrameworkPublicationToCom.kingmakers.analyticsRepository")?.mustRunAfter("buildIosFramework")
+}
+
+
 dependencies {
     implementation(project(":shared:features"))
 }
